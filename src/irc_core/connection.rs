4f5e3e41aaa011036adcfb03f6d98861b233818d
Matus-Abrhan <matusabrhan27@gmail.com>
2024-10-27 12:39:57 +0100

    move write_response to connction

--- src/irc_core/connection.rs
+++ src/irc_core/connection.rs
@@ -1,10 +1,13 @@
 use std::{io::Cursor, net::SocketAddr};
-
+use std::sync::Arc;
+use std::collections::HashMap;
 use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpStream};
+use tokio::sync::{mpsc, Mutex};
 use bytes::{Buf, BytesMut};
 use log::{info, warn};
 
 use crate::irc_core::message::Message;
+use crate::irc_core::command::Command;
 use crate::irc_core::error::IRCError;
 use crate::irc_core::numeric::ErrorReply;
 
@@ -13,6 +16,7 @@ enum JoinedError {
     ErrorReply(ErrorReply),
 }
 
+#[derive(Clone, Copy)]
 pub enum RegistrationState {
     None = 0,
     PassReceived = 1,
@@ -21,6 +25,7 @@ pub enum RegistrationState {
     Registered = 4,
 }
 
+#[derive(Clone)]
 pub struct State {
     pub registration_state: RegistrationState,
     pub nickname: String,
@@ -31,15 +36,17 @@ pub struct State {
 pub struct Connection {
     pub stream: TcpStream,
     pub address: SocketAddr,
+    pub task_sender_map: Arc<Mutex<HashMap<String, mpsc::Sender<Result<Option<Message>, IRCError>>>>>,
     pub buffer: BytesMut,
     pub state: State,
 }
 
 impl Connection {
-    pub fn new(stream: TcpStream, address: SocketAddr) -> Connection {
+    pub fn new(stream: TcpStream, address: SocketAddr, task_sender_map: Arc<Mutex<HashMap<String, mpsc::Sender<Result<Option<Message>, IRCError>>>>>) -> Connection {
         Connection {
             stream,
             address,
+            task_sender_map,
             buffer: BytesMut::with_capacity(1024 * 2),
             state: State{registration_state: RegistrationState::None,
                 nickname: String::new(), username: String::new(), realname: String::new()
@@ -126,5 +133,106 @@ impl Connection {
             Err(e) => return Err(JoinedError::IRCError(e)),
         };
 
+    }
+
+    pub async fn write_response(&mut self, message: Message) {
+        match &message.command {
+            Command::Pass{password} => {
+                match self.state.registration_state {
+                    RegistrationState::None => {
+                        if password == "blabla" { // Match connection password
+                            self.state.registration_state = RegistrationState::PassReceived;
+                        } else {
+                            self.write_error(&ErrorReply::PasswdMismatch).await;
+                        }
+                    },
+                    _ => {
+                        self.write_error(&ErrorReply::AlreadyRegistred).await;
+                    },
+                };
+            },
+
+            Command::Nick{nickname} => {
+                let new_reg_state: Option<RegistrationState> = match self.state.registration_state {
+                    RegistrationState::PassReceived => {
+                        Some(RegistrationState::NickReceived)
+                    },
+                    RegistrationState::UserReceived => {
+                        Some(RegistrationState::Registered)
+                    },
+                    _ => {
+                        None
+                    },
+                };
+                let new_nick = nickname.to_string();
+                // TODO: check if contains disallowed characters (ERR_ERRONEUSNICKNAME)
+                if false {
+                    self.write_error(&ErrorReply::ErroneusNickname).await;
+                    return;
+                }
+                // TODO: check if nick in useed on network (ERR_NICKNAMEINUSE)
+                // TODO: ERR_NICKCOLLISION ???
+
+                self.state.nickname = new_nick;
+                if let Some(reg_state) = new_reg_state {
+                    self.state.registration_state = reg_state;
+                }
+            },
+
+            Command::User{user, realname, ..} => {
+                match self.state.registration_state {
+                    RegistrationState::PassReceived => {
+                        self.state.registration_state = RegistrationState::UserReceived;
+                    },
+                    RegistrationState::NickReceived=> {
+                        self.state.registration_state = RegistrationState::Registered;
+                    },
+                    _ => {
+                        self.write_error(&ErrorReply::AlreadyRegistred).await;
+                    }
+                }
+                self.state.username = user.to_string();
+                self.state.realname = realname.to_string();
+            },
+
+            Command::Ping{token} => {
+                self.write_message(&Message{
+                    prefix: None,
+                    command: Command::Pong{ server: None, token: token.to_string()}
+                }).await
+            },
+
+            // Command::PrivMsg{targets, text} => {
+            //         let task_sender_map = self.task_sender_map.lock().await;
+            //         for value in task_sender_map.values() {
+            //             value.send().await
+            //         }
+            //         drop(task_sender_map)
+            // },
+
+            // Command::Join{channels, ..} => {
+            //     let mut channel_db = self.channel_db.lock().await;
+            //     let mut messages: Vec<Message> = Vec::new();
+            //     for (_idx, channel) in channels.split(',').enumerate() {
+            //         channel_db.push(Channel{
+            //             name: channel.to_string(),
+            //             members: Vec::from([self.connection.state.nickname.clone()]),
+            //             flags: Vec::new(),
+            //         });
+            //         messages.push(Message{
+            //             prefix: Some(self.connection.state.username.clone()),
+            //             command: Command::Join{channels: channel.to_string(), keys: None},
+            //         })
+            //     };
+            //     for message in messages {
+            //         self.connection.write_message(&message).await;
+            //     }
+            //     drop(channel_db);
+            //
+            //
+            // },
+
+            _ => {},
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,23 @@
 use std::future::Future;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use log::{info, warn, error};
+use log::{info, warn};
+use std::collections::HashMap;
 
-use tokio::io::AsyncWriteExt;
+// use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::{broadcast, mpsc, Mutex};
 use tokio::time::{self, Duration};
 
-use crate::irc_core::connection::{Connection, RegistrationState};
-use crate::irc_core::command::Command;
+use crate::irc_core::connection::{Connection, State};
 use crate::irc_core::message::Message;
 use crate::irc_core::channel::Channel;
-use crate::irc_core::numeric::ErrorReply;
 use crate::irc_core::error::IRCError;
 
-// TODO: should std::Mutex or tokio Mutex be used ?
-// type ChannelDb = Arc<tokio::sync::Mutex<Vec<Channel>>>;
-type ChannelDb = Arc<Mutex<Vec<Channel>>>;
+// type ChannelDb = Arc<Mutex<Vec<Channel>>>;
+// type ConnectionDb = Arc<Mutex<Vec<State>>>;
+
+
 // NOTE: could by more granular?
 // Arc<Vec<Mutex<Channel>>>
 
@@ -25,7 +25,9 @@ const BACKOFF_LIMIT: u64 = 64;
 
 struct Listener {
     listener: TcpListener,
-    channel_db: ChannelDb,
+    // channel_db: ChannelDb,
+    // connection_db: ConnectionDb,
+    task_sender_map: Arc<Mutex<HashMap<String, mpsc::Sender<Result<Option<Message>, IRCError>>>>>,
     notify_shutdown: broadcast::Sender<()>,
     shutdown_complete_tx: mpsc::Sender<()>,
     shutdown_complete_rx: mpsc::Receiver<()>,
@@ -33,7 +35,9 @@ struct Listener {
 
 struct Handler {
     connection: Connection,
-    channel_db: ChannelDb,
+    // channel_db: ChannelDb,
+    // connection_db: ConnectionDb,
+    task_receiver: mpsc::Receiver<Result<Option<Message>, IRCError>>,
     shutdown: Shutdown,
     _shutdown_complete: mpsc::Sender<()>,
 }
@@ -48,10 +52,15 @@ impl Listener {
 
         loop {
             let (stream, address) = self.accept().await?;
+            let (task_tx, task_rx) = mpsc::channel(1);
+            let mut map = self.task_sender_map.lock().await;
+            map.insert(address.to_string(), task_tx);
+            drop(map);
             let mut handler = Handler{
-                // db: self.db.clone(),
-                channel_db: self.channel_db.clone(),
-                connection: Connection::new(stream, address),
+                // channel_db: self.channel_db.clone(),
+                // connection_db: self.connection_db.clone(),
+                task_receiver: task_rx,
+                connection: Connection::new(stream, address, self.task_sender_map.clone()),
                 shutdown: Shutdown::new(self.notify_shutdown.subscribe()),
                 _shutdown_complete: self.shutdown_complete_tx.clone(),
             };
@@ -89,6 +98,12 @@ impl Handler {
         while !self.shutdown.is_shutdown() {
             let received = tokio::select! {
                 res = self.connection.read_message() => res,
+                res = self.task_receiver.recv() => {
+                    match res {
+                        Some(res) => res,
+                        None => Ok(None),
+                    }
+                },
                 _ = self.shutdown.recv() => {
                     info!("Server quit");
                     return Err(());
@@ -98,7 +113,7 @@ impl Handler {
                 Ok(maby_message) => {
                     match maby_message {
                         Some(message) => {
-                            self.write_response(message).await
+                            self.connection.write_response(message).await
                         },
                         None => {
 
@@ -119,98 +134,6 @@ impl Handler {
             };
         }
         Ok(())
-    }
-
-    async fn write_response(&mut self, message: Message) {
-        match &message.command {
-            Command::Pass{password} => {
-                match self.connection.state.registration_state {
-                    RegistrationState::None => {
-                        if password == "blabla" { // Match connection password
-                            self.connection.state.registration_state = RegistrationState::PassReceived;
-                        } else {
-                            self.connection.write_error(&ErrorReply::PasswdMismatch).await;
-                        }
-                    },
-                    _ => {
-                        self.connection.write_error(&ErrorReply::AlreadyRegistred).await;
-                    },
-                };
-            },
-
-            Command::Nick{nickname} => {
-                let new_reg_state: Option<RegistrationState> = match self.connection.state.registration_state {
-                    RegistrationState::PassReceived => {
-                        Some(RegistrationState::NickReceived)
-                    },
-                    RegistrationState::UserReceived => {
-                        Some(RegistrationState::Registered)
-                    },
-                    _ => {
-                        None
-                    },
-                };
-                let new_nick = nickname.to_string();
-                // TODO: check if contains disallowed characters (ERR_ERRONEUSNICKNAME)
-                if false {
-                    self.connection.write_error(&ErrorReply::ErroneusNickname).await;
-                    return;
-                }
-                // TODO: check if nick in useed on network (ERR_NICKNAMEINUSE)
-                // TODO: ERR_NICKCOLLISION ???
-
-                self.connection.state.nickname = new_nick;
-                if let Some(reg_state) = new_reg_state {
-                    self.connection.state.registration_state = reg_state;
-                }
-            },
-
-            Command::User{user, realname, ..} => {
-                match self.connection.state.registration_state {
-                    RegistrationState::PassReceived => {
-                        self.connection.state.registration_state = RegistrationState::UserReceived;
-                    },
-                    RegistrationState::NickReceived=> {
-                        self.connection.state.registration_state = RegistrationState::Registered;
-                    },
-                    _ => {
-                        self.connection.write_error(&ErrorReply::AlreadyRegistred).await;
-                    }
-                }
-                self.connection.state.username = user.to_string();
-                self.connection.state.realname = realname.to_string();
-            },
-
-            Command::Ping{token} => {
-                self.connection.write_message(&Message{
-                    prefix: None,
-                    command: Command::Pong{ server: None, token: token.to_string()}
-                }).await
-            },
-
-            Command::Join{channels, ..} => {
-                let mut channel_db = self.channel_db.lock().await;
-                let mut messages: Vec<Message> = Vec::new();
-                for (_idx, channel) in channels.split(',').enumerate() {
-                    channel_db.push(Channel{
-                        name: channel.to_string(),
-                        members: Vec::from([self.connection.state.nickname.clone()]),
-                        flags: Vec::new(),
-                    });
-                    messages.push(Message{
-                        prefix: Some(self.connection.state.username.clone()),
-                        command: Command::Join{channels: channel.to_string(), keys: None},
-                    })
-                };
-                for message in messages {
-                    self.connection.write_message(&message).await;
-                }
-                drop(channel_db);
-
-
-            },
-            _ => {}
-        }
     }
 }
 
@@ -239,8 +162,9 @@ pub async fn run(listener: TcpListener, shutdown: impl Future) -> Result<(), ()>
 
     let mut server = Listener {
         listener,
-        // channel_db: Arc::new(tokio::sync::Mutex::new(Vec::new())),
-        channel_db: Arc::new(Mutex::new(Vec::new())),
+        // channel_db: Arc::new(Mutex::new(Vec::new())),
+        // connection_db: Arc::new(Mutex::new(Vec::new())),
+        task_sender_map: Arc::new(Mutex::new(HashMap::new())),
         notify_shutdown,
         shutdown_complete_tx,
         shutdown_complete_rx
